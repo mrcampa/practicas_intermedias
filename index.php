2b39032bb538657c6ceb31807d9df7af4656e63c
Diego <gomezefdiego@gmail.com>
2020-05-08 09:33:50 -0600

    config

---
+++ index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DEVED</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/keyframes.css">
+    <link rel="stylesheet" href="css/banner.css">
+    <link rel="stylesheet" href="css/carlos.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Inicio Menú - navbar de bootstrap  fixed-top-->
+    <nav class="navbar main-navbar navbar-expand-lg navbar-light bg-light navbar-gradiente">
+        <!-- con "fixed-top" se queda estaticamente arriba-->
+        <div class="container">
+            <nav class="">
+                <a class="navbar-brand" href="desktop1.html">
+                    <img src="img/navbardevev.png" width="147" height="42" alt="DEDEV.icon">
+                </a>
+            </nav>
+            <!-- botton para hacerlo responsive-->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-nav main-navbar-nav">
+                    <a class="nav-item nav-link main-nav-link" href="#">PROYECTOS     <span class="sr-only">(current)</span>    </a>
+                    <a class="nav-item nav-link main-nav-link" href="#">DEDEV</a>
+                    <a class="nav-item nav-link main-nav-link" href="#">NOTICIAS</a>
+                    <a class="nav-item nav-link main-nav-link" href="#">AULAS VIRTUALES</a>
+                    <a class="nav-item nav-link main-nav-link" href="#">RECURSOS MULTIMEDIA</a>
+                    <form class="form-inline">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="Buscar" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Fin Menú - navbar de bootstrap-->
+
+    <!-- Inicio del header-->
+    <header class="main-header-gradiente main-header">
+        <!-- Imagen de cambio  -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="content-line">
+                        <img src="img/minidot.png" alt="frame2" class="img_fluid d-none d-sm-block circle-top1">
+                        <img src="img/vertline1.png" alt="frame2" class="img_fluid d-none d-sm-block linea-buttom">
+                    </div>
+                    <div class="content-line">
+                        <img src="img/minidot.png" alt="frame2" class="img_fluid d-none d-sm-block circle-top2">
+                        <img src="img/vertline2.png" alt="frame2" class="img_fluid d-none d-sm-block linea-buttom">
+                    </div>
+                    <div class="content-line">
+                        <img src="img/minidot.png" alt="frame2" class="img_fluid d-none d-sm-block circle-top3">
+                        <img src="img/vertline3.png" alt="frame2" class="img_fluid d-none d-sm-block linea-buttom">
+                    </div>
+                </div>
+                <div class="col-md-5 text-left justify-content-center align-self-center">
+                    <H1><strong></strong><span class="color-text-1">INNOVANDO EN <br> LA </span><span class="color-text-2">EDUCACION SUPERIOR</span></H1>
+                </div>
+                <div class="col-md-4">
+                    <div class="ubication1">
+                        <img src="img/man_computer.png" alt="man_computer" class="img_fluid">
+                    </div>
+                    <div class="ubication2">
+                        <img src="img/Frame4.png" alt="frame4" class="img_fluid">
+                    </div>
+                    <div class="ubication3">
+                        <!-- d-none and d-sm-bloque, hacen que la imagen desaparezca cuando la pantalla es muy pequeña -->
+                        <img src="img/Frame.png" alt="frame" class="img_fluid">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Fin del header-->
+
+    <!-- FEATURES -->
+    <section>
+        <div class="container features">
+            <div class="row p-5">
+                <div class="inline">
+                    <img src="img/virrete.png" alt="frame2" class="img_fluid virrete">
+                </div>
+                <div class="inline">
+                    <h1 class="diplomado">DIPLOMADOS</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-1" style="height: 0px;">&nbsp;</div>
+                <div class="col-md-2">
+                    <div class="card text-center card-back-sec">
+                        <div class="card-body text-center justify-content-center align-self-center">
+                            <img src="img/comp-mouse.png" alt="comp-mouse" class="img_fluid">
+                            <h2>Diplomado</h2>
+                            <p>en tutoria virtual</p>
+                            <a href="#">Ingresar</a>
+                        </div>
+                    </div>
+                    <div class="ubication1">
+                        <img src="img/minidot.png" alt="frame2" class="img_fluid">
+                    </div>
+                    <div class="ubication2">
+                        <img src="img/vertline3.png" alt="frame2" class="img_fluid">
+                    </div>
+                    <div class="ubication3">
+                        <img src="img/Frame2.png" alt="frame2" class="img_fluid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="card text-center card-back-sec">
+                        <div class="card-body text-center justify-content-center align-self-center">
+                            <img src="img/comp-mouse.png" alt="comp-mouse" class="img_fluid">
+                            <h2>Diplomado</h2>
+                            <p>en tutoria virtual</p>
+                            <a href="#">Ingresar</a>
+                        </div>
+                    </div>
+                    <div class="ubication1">
+                        <img src="img/minidot.png" alt="frame2" class="img_fluid">
+                    </div>
+                    <div class="ubication2">
+                        <img src="img/vertline3.png" alt="frame2" class="img_fluid">
+                    </div>
+                    <div class="ubication3">
+                        <img src="img/Frame2.png" alt="frame2" class="img_fluid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="card text-center card-back-sec">
+                        <div class="card-body text-center justify-content-center align-self-center">
+                            <img src="img/comp-cuadro.png" alt="comp-mouse" class="img_fluid">
+                            <h2>Diplomado</h2>
+                            <p>en tutoria virtual</p>
+                            <a href="#">Ingresar</a>
+                        </div>
+                    </div>
+                    <div class="ubication1">
+                        <img src="img/minidot.png" alt="frame2" class="img_fluid">
+                    </div>
+                    <div class="ubication2">
+                        <img src="img/vertline3.png" alt="frame2" class="img_fluid">
+                    </div>
+                    <div class="ubication3">
+                        <img src="img/Frame2.png" alt="frame2" class="img_fluid">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="card text-center card-back-sec">
+                        <div class="card-body text-center justify-content-center align-self-center">
+                            <img src="img/pencil.png" alt="comp-mouse" class="img_fluid">
+                            <h2>Diplomado</h2>
+                            <p>en tutoria virtual</p>
+                            <a href="#">Ingresar</a>
+                        </div>
+                    </div>
+                    <div class="ubication1">
+                        <img src="img/minidot.png" alt="frame2" class="img_fluid">
+                    </div>
+                    <div class="ubication2">
+                        <img src="img/vertline3.png" alt="frame2" class="img_fluid">
+                    </div>
+                    <div class="ubication3">
+                        <img src="img/Frame2.png" alt="frame2" class="img_fluid">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="face-box  text-center justify-content-center align-self-center">
+                        <div style="display: inline-block;">
+                            <img src="img/buttom-con.png" alt="buttom-con" class="img_fluid">
+                        </div>
+                        <div style="display: inline-block;">
+                            <h2>Live Facebook</h2>
+                            <p>En vivo en este momento</p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="face-cont  text-center justify-content-center align-self-center">
+                        <div>
+                            <h2>¿Que hay de nuevo?</h2>
+                        </div>
+                        <div class="box-gray">&nbsp; </div><br>
+                        <div class="box-gray">&nbsp;</div><br>
+                        <div class="box-gray">&nbsp;</div><br>
+                    </div>
+                </div>
+            </div>
+            <!--
+            <div class="col-md-1 bar-side ">
+                <div class="bar-side-img ">
+                    &nbsp;
+                </div>
+            </div> -->
+        </div>
+    </section>
+    <!-- END FEATURES -->
+
+    <!-- BEGIN ABOUT -->
+    <section class="about">
+        <div class="container">
+            <div class="row">
+                <div class="space">&nbsp;</div>
+                <div class="col-md-5  text-left justify-content-left align-self-left">
+                    <div class="p-2">
+                        <div class="inline">
+                            <img src="img/person.png" alt="person" class="img_fluid">
+                        </div>
+                        <div class="inline">
+                            <strong>¿QUIÉNES SOMOS?</strong>
+                        </div>
+                    </div>
+                    <br><br>
+                    <h4>“Somos la instancia responsable de implementar e innovar prácticas educativas a través de la gestión del conocimiento y la investigación”</h4><br>
+                    <span>así como del aprendizaje en ambientes virtuales utilizando tecnologías educativas como recursos didáctico-metodológicos, para la consecución de los fines de la Universidad de San Carlos de Guatemala. Trabajamos en el desarrollo de las modalidades de enseñanza-aprendizaje (abierta, a distancia, virtual y en línea) para los programas oficiales de la Administración Central y el acompañamiento de la implementación en las distintas unidades académicas de la Universidad.</span><br><br>
+                    <img src="img/bottom-bar-cel.png" alt="bottom-bar" class="img_fluid" style="margin-bottom: 20px;">
+                </div>
+                <div class="col-md-4">
+                    <div class="ubication1 ">
+                        <img src="img/man_signal.png" alt="man_signal" class="img_fluid">
+                    </div>
+                    <div class="ubication2 ">
+                        <img src="img/Frame.png " alt="frame " class="img_fluid">
+                    </div>
+                    <div class="ubication3 ">
+                        <img src="img/Frame1.png " alt="frame1 " class="img_fluid">
+                    </div>
+                    <div class="ubication4 ">
+                        <img src="img/point.png " alt="point " class="img_fluid">
+                    </div>
+                </div>
+                <div class="col-md-2 " style="height: 0px; ">&nbsp;</div>
+            </div>
+            <div class="imageleft " style="top: 20%;">&nbsp;</div>
+            <div class="imageleft " style="top: 35%;">&nbsp;</div>
+            <div class="imageleft " style="top: 50%;">&nbsp;</div>
+            <div class="imageleft " style="top: 65%;">&nbsp;</div>
+            <div class="imageleft " style="top: 80%;">&nbsp;</div>
+            <div class="imageright " style="top: 20%;">&nbsp;</div>
+            <div class="imageright " style="top: 35%;">&nbsp;</div>
+            <div class="imageright " style="top: 50%;">&nbsp;</div>
+            <div class="imageright " style="top: 65%;">&nbsp;</div>
+            <div class="imageright " style="top: 80%;">&nbsp;</div>
+        </div>
+    </section>
+    <!-- END ABOUT -->
+
+
+
+
+    <section>
+        <div class="container ">
+            <div class="row py-5 diplo-1 ">
+                <div class="col-md-4 ">
+                    <h1>VIDEOTECA</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-1">
+                    <img src="img/bottombardots.png" alt="bottombardots" class="img_fluid">
+                </div>
+                <div class="col-md-1 px-5 py-1">
+                    <img src="img/bottombardevev.png" alt="bottombardots" class="img_fluid">
+                </div>
+                <div class="col-md-6">
+                    <a href="#">¿QUIÉNES SOMOS?</a>
+                    <a href="#">CONTACTO</a>
+                    <a href="#">PREGUNTAS FRECUENTES</a>
+                </div>
+                <div class="col-md-2">
+                    <img src="img/mediasocial.png" alt="mediasocial" class="img_fluid">
+                </div>
+                <div class="col-md-1">
+                    <img src="img/bottombardots.png" alt="bottombardots" class="img_fluid">
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="jquery/js/jquery-3.4.1.min.js "></script>
+    <script src="popper/js/popper.min.js "></script>
+    <script src="bootstrap/js/bootstrap.min.js "></script>
+    <script src="js/index.js "></script>
+</body>
+
+</html>
