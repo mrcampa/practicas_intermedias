7619c1a0712f5389c6c01c50c4934bc3611d5225
Diego <gomezefdiego@gmail.com>
2020-05-08 11:25:32 -0600

    configuration, scripts not working

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function add_theme_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap/css/bootstrap.min.css');
+    wp_enqueue_style( 'style1', get_template_directory_uri() . '/css/banner.css');
+    wp_enqueue_style( 'style2', get_template_directory_uri() . '/css/keyframes.css');
+    wp_enqueue_style( 'style3', get_template_directory_uri() . '/css/main.css');
+    wp_enqueue_style( 'style4', get_template_directory_uri() . '/css/carlos.css');
+    wp_enqueue_style( 'style5', get_template_directory_uri() . '/css/herbert.css');
+    wp_enqueue_style( 'style6', get_template_directory_uri() . '/css/herbert2.css');
+    wp_enqueue_style( 'style7', get_template_directory_uri() . '/css/style.css');
+    wp_enqueue_style( 'style8', get_template_directory_uri() . '/css/dedev.css');
+    
+    wp_enqueue_script( 'popper', get_template_directory_uri() . 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array ( 'jquery' ), 1.16, true);
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array ( 'popper' ), 4.3, true);
+  }
+  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
--- index.php
+++ index.php
@@ -6,11 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DEVED</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/keyframes.css">
-    <link rel="stylesheet" href="css/banner.css">
-    <link rel="stylesheet" href="css/carlos.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <?php wp_head(); ?>
 </head>
 
 <body>
@@ -298,6 +295,7 @@
     <script src="popper/js/popper.min.js "></script>
     <script src="bootstrap/js/bootstrap.min.js "></script>
     <script src="js/index.js "></script>
+    <?php wp_footer(); ?>
 </body>
 
 </html>
